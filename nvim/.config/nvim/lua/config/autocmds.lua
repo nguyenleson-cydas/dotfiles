2ea2162f8cb04f61cc6e0be8b858568ad9021e8f
nguyenleson-cydas <nguyenle.son@cydas.com>
2025-11-16 09:24:45 +0700

    wip

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -16,29 +16,33 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPost", "FileReadPost", "BufNew
   end,
 })
 
--- Auto-insert date/time header when entering note files
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = { "*/notes/*.md", "*/notes/**/*.md", "*quick-note.md" },
+-- Restore tmux pane title when exiting nvim
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  pattern = "*",
   callback = function()
-    -- Only insert if buffer was just created or is empty
-    local line_count = vim.api.nvim_buf_line_count(0)
-    local is_empty = line_count == 1 and vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] == ""
-
-    local date_time = tostring(os.date("## %Y-%m-%d %H:%M:%S"))
-
-    if is_empty then
-      vim.api.nvim_buf_set_lines(0, 0, 0, false, { date_time, "", "" })
-      -- Move cursor to the third line for writing
-      vim.api.nvim_win_set_cursor(0, { 3, 0 })
-    else
-      -- If file has content, append a new timestamped section at the end
-      local last_line = vim.api.nvim_buf_get_lines(0, -2, -1, false)[1]
-      -- Only add separator and timestamp if the last line is not empty
-      if last_line ~= "" then
-        vim.api.nvim_buf_set_lines(0, line_count, line_count, false, { "", "---", "", date_time, "", "" })
-        -- Move cursor to the end
-        vim.api.nvim_win_set_cursor(0, { line_count + 6, 0 })
-      end
-    end
+    vim.fn.system("tmux select-pane -T ''")
   end,
 })
+
+-- User command to insert date/time timestamp
+vim.api.nvim_create_user_command("InsertTimestamp", function()
+  local line_count = vim.api.nvim_buf_line_count(0)
+  local is_empty = line_count == 1 and vim.api.nvim_buf_get_lines(0, 0, 1, false)[1] == ""
+
+  local date_time = tostring(os.date("## %Y-%m-%d %H:%M:%S"))
+
+  if is_empty then
+    vim.api.nvim_buf_set_lines(0, 0, 0, false, { date_time, "", "" })
+    -- Move cursor to the third line for writing
+    vim.api.nvim_win_set_cursor(0, { 3, 0 })
+  else
+    -- If file has content, append a new timestamped section at the end
+    local last_line = vim.api.nvim_buf_get_lines(0, -2, -1, false)[1]
+    -- Only add separator and timestamp if the last line is not empty
+    if last_line ~= "" then
+      vim.api.nvim_buf_set_lines(0, line_count, line_count, false, { "", "---", "", date_time, "", "" })
+      -- Move cursor to the end
+      vim.api.nvim_win_set_cursor(0, { line_count + 6, 0 })
+    end
+  end
+end, { desc = "Insert timestamp header" })
