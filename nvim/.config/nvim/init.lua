15508b7c2abe59c875edcd8bd6c4e0a704f9fbc5
nguyenleson-cydas <nguyenle.son@cydas.com>
2025-12-25 20:48:13 +0700

    wip

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -26,6 +26,10 @@ vim.pack.add {
     src = 'https://github.com/nvim-treesitter/nvim-treesitter.git',
     version = 'master',
   },
+  {
+    src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git',
+    version = 'master',
+  },
   'https://github.com/craftzdog/solarized-osaka.nvim.git',
   'https://github.com/christoomey/vim-tmux-navigator.git',
   'https://github.com/NMAC427/guess-indent.nvim.git',
@@ -505,6 +509,26 @@ vim.keymap.set('n', '<leader>sS', function()
   Snacks.picker.lsp_workspace_symbols()
 end, { desc = 'LSP Workspace Symbols' })
 
+-- Terminal (cwd)
+vim.keymap.set({ 'n', 't' }, '<leader>fT', function()
+  Snacks.terminal()
+end, { desc = 'Terminal (cwd)', noremap = true })
+
+-- Terminal (Root Dir)
+vim.keymap.set({ 'n', 't' }, '<leader>ft', function()
+  Snacks.terminal(nil, { cwd = vim.fn.getcwd() })
+end, { desc = 'Terminal (Root Dir)', noremap = true })
+
+-- Toggle Terminal
+vim.keymap.set({ 'n', 't' }, '<c-/>', function()
+  Snacks.terminal()
+end, { desc = 'Toggle Terminal', noremap = true })
+
+-- which_key_ignore (Root Dir) — normal mode
+vim.keymap.set({ 'n', 't' }, '<c-_>', function()
+  Snacks.terminal(nil, { cwd = vim.fn.getcwd() })
+end, { desc = 'which_key_ignore', noremap = true })
+
 require('lazydev').setup {
   library = {
     -- Load luvit types when the `vim.uv` word is found
@@ -773,19 +797,8 @@ vim.api.nvim_create_autocmd('PackChanged', {
 })
 
 require('todo-comments').setup { signs = false }
--- Better Around/Inside textobjects
---
--- Examples:
---  - va)  - [V]isually select [A]round [)]paren
---  - yinq - [Y]ank [I]nside [N]ext [Q]uote
---  - ci'  - [C]hange [I]nside [']quote
-require('mini.ai').setup { n_lines = 500 }
 
--- Add/delete/replace surroundings (brackets, quotes, etc.)
---
--- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
--- - sd'   - [S]urround [D]elete [']quotes
--- - sr)'  - [S]urround [R]eplace [)] [']
+require('mini.ai').setup { n_lines = 500 }
 require('mini.surround').setup()
 require('mini.icons').setup()
 require('mini.pairs').setup()
@@ -837,6 +850,89 @@ require('nvim-treesitter.configs').setup {
     end,
   },
   indent = { enable = true, disable = { 'ruby' } },
+}
+
+require('nvim-treesitter.configs').setup {
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        -- You can optionally set descriptions to the mappings (used in the desc parameter of
+        -- nvim_buf_set_keymap) which plugins like which-key display
+        ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+        -- You can also use captures from other query groups like `locals.scm`
+        ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
+      },
+      -- You can choose the select mode (default is charwise 'v')
+      --
+      -- Can also be a function which gets passed a table with the keys
+      -- * query_string: eg '@function.inner'
+      -- * method: eg 'v' or 'o'
+      -- and should return the mode ('v', 'V', or '<c-v>') or a table
+      -- mapping query_strings to modes.
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V', -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
+      },
+      -- If you set this to `true` (default is `false`) then any textobject is
+      -- extended to include preceding or succeeding whitespace. Succeeding
+      -- whitespace has priority in order to act similarly to eg the built-in
+      -- `ap`.
+      --
+      -- Can also be a function which gets passed a table with the keys
+      -- * query_string: eg '@function.inner'
+      -- * selection_mode: eg 'v'
+      -- and should return true or false
+      include_surrounding_whitespace = true,
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = { query = '@class.outer', desc = 'Next class start' },
+        --
+        -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+        [']o'] = '@loop.*',
+        -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+        --
+        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+        [']s'] = { query = '@local.scope', query_group = 'locals', desc = 'Next scope' },
+        [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+      -- Below will go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      -- Make it even more gradual by adding multiple queries and regex.
+      goto_next = {
+        [']d'] = '@conditional.outer',
+      },
+      goto_previous = {
+        ['[d'] = '@conditional.outer',
+      },
+    },
+  },
 }
 
 vim.api.nvim_create_autocmd('PackChanged', {
