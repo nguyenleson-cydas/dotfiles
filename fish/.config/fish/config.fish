762a8bf0909d83b94e3dbf50abedd69c25dc4a99
nguyenleson-cydas <nguyenle.son@cydas.com>
2026-01-04 11:08:56 +0700

    wip

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -71,6 +71,21 @@ if status --is-login
     --color=separator:#063540 \
     --color=spinner:#c94c16 \
     "
+
+    # ==================== ASDF CONFIGURATION CODE  ====================
+
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
+    end
+
+    # Do not use fish_add_path (added in Fish 3.2) because it
+    # potentially changes the order of items in PATH
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
+    end
+    set --erase _asdf_shims
 end
 
 if status --is-interactive
