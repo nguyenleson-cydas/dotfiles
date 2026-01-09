b322f855856cee45296f5c04500720ab73ff43e4
nguyenleson-cydas <nguyenle.son@cydas.com>
2026-01-10 00:06:43 +0900

    wip

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -14,19 +14,17 @@ require('mason-nvim-dap').setup {
     'php',
   },
 }
-local dap, dapui = require 'dap', require 'dapui'
+local dap = require 'dap'
+local dapui = require 'dapui'
 dapui.setup {}
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
+dap.listeners.after.event_initialized['dapui_config'] = function()
+  dapui.open {}
 end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
+dap.listeners.before.event_terminated['dapui_config'] = function()
+  dapui.close {}
 end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
+dap.listeners.before.event_exited['dapui_config'] = function()
+  dapui.close {}
 end
 dap.adapters.php = {
   type = 'executable',
@@ -46,42 +44,76 @@ dap.configurations.php = {
   },
 }
 
-vim.keymap.set('n', '<F5>', function()
-  require('dap').continue()
-end, { desc = 'Debug: Start/Continue' })
+local widgets = require 'dap.ui.widgets'
 
-vim.keymap.set('n', '<S-F5>', function()
-  require('dap').terminate()
-end, { desc = 'Debug: Stop' })
+vim.keymap.set('n', '<leader>dB', function()
+  dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+end, { desc = 'Breakpoint Condition' })
 
-vim.keymap.set('n', '<C-S-F5>', function()
-  require('dap').restart()
-end, { desc = 'Debug: Restart' })
+vim.keymap.set('n', '<leader>db', function()
+  dap.toggle_breakpoint()
+end, { desc = 'Toggle Breakpoint' })
 
-vim.keymap.set('n', '<F6>', function()
-  require('dap').pause()
-end, { desc = 'Debug: Pause' })
+vim.keymap.set('n', '<leader>dc', function()
+  dap.continue()
+end, { desc = 'Run/Continue' })
 
-vim.keymap.set('n', '<F10>', function()
-  require('dap').step_over()
-end, { desc = 'Debug: Step Over' })
+vim.keymap.set('n', '<leader>dC', function()
+  dap.run_to_cursor()
+end, { desc = 'Run to Cursor' })
 
-vim.keymap.set('n', '<F11>', function()
-  require('dap').step_into()
-end, { desc = 'Debug: Step Into' })
+vim.keymap.set('n', '<leader>dg', function()
+  dap.goto_()
+end, { desc = 'Go to Line (No Execute)' })
 
-vim.keymap.set('n', '<S-F11>', function()
-  require('dap').step_out()
-end, { desc = 'Debug: Step Out' })
+vim.keymap.set('n', '<leader>di', function()
+  dap.step_into()
+end, { desc = 'Step Into' })
 
-vim.keymap.set('n', '<F9>', function()
-  require('dap').toggle_breakpoint()
-end, { desc = 'Debug: Toggle Breakpoint' })
+vim.keymap.set('n', '<leader>dj', function()
+  dap.down()
+end, { desc = 'Down' })
 
-vim.keymap.set('n', '<F7>', function()
-  require('dapui').toggle()
-end, { desc = 'Debug: See last session result.' })
+vim.keymap.set('n', '<leader>dk', function()
+  dap.up()
+end, { desc = 'Up' })
 
-vim.keymap.set('n', '<leader>?', function()
-  require('dap').repl.toggle()
+vim.keymap.set('n', '<leader>dl', function()
+  dap.run_last()
+end, { desc = 'Run Last' })
+
+vim.keymap.set('n', '<leader>do', function()
+  dap.step_out()
+end, { desc = 'Step Out' })
+
+vim.keymap.set('n', '<leader>dO', function()
+  dap.step_over()
+end, { desc = 'Step Over' })
+
+vim.keymap.set('n', '<leader>dP', function()
+  dap.pause()
+end, { desc = 'Pause' })
+
+vim.keymap.set('n', '<leader>dr', function()
+  dap.repl.toggle()
 end, { desc = 'Toggle REPL' })
+
+vim.keymap.set('n', '<leader>ds', function()
+  dap.session()
+end, { desc = 'Session' })
+
+vim.keymap.set('n', '<leader>dt', function()
+  dap.terminate()
+end, { desc = 'Terminate' })
+
+vim.keymap.set('n', '<leader>dw', function()
+  widgets.hover()
+end, { desc = 'Widgets' })
+
+vim.keymap.set('n', '<leader>du', function()
+  dapui.toggle {}
+end, { desc = 'Dap UI' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>de', function()
+  dapui.eval()
+end, { desc = 'Eval' })
