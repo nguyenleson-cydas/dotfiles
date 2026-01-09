fd6a56f42aefe49faa6b175605541ae846f65544
nguyenleson-cydas <nguyenle.son@cydas.com>
2026-01-10 01:11:36 +0900

    wip

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -15,7 +15,7 @@ require('snacks').setup {
   words = { enabled = true },
   styles = {
     notification = {
-      -- wo = { wrap = true } -- Wrap notifications
+      wo = { wrap = true } -- Wrap notifications
     },
   },
 }
@@ -192,4 +192,15 @@ vim.keymap.set('n', '<leader>gP', function()
 end, { desc = 'GitHub Pull Requests (all)' })
 
 -- Toggle
+-- Create some toggle mappings
+Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
+Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
+Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
+Snacks.toggle.diagnostics():map '<leader>ud'
+Snacks.toggle.line_number():map '<leader>ul'
+Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
+Snacks.toggle.treesitter():map '<leader>uT'
+Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
 Snacks.toggle.inlay_hints():map '<leader>uh'
+Snacks.toggle.indent():map '<leader>ug'
+Snacks.toggle.dim():map '<leader>uD'
--- nvim/.config/nvim/lua/plugins/transparent.lua
+++ nvim/.config/nvim/lua/plugins/transparent.lua
@@ -2,14 +2,17 @@ vim.pack.add { 'https://github.com/xiyaowong/transparent.nvim' }
 require('transparent').setup {
   tranfarency = true,
   exclude_groups = {
-    -- 'CursorLine',
+    'CursorLine',
   },
   extra_groups = {
     'NormalFloat',
-    'FloatBorder',
-    'SnacksPickerBorder',
   },
 }
 require('transparent').clear_prefix 'lualine'
 require('transparent').clear_prefix 'Blink'
 require('transparent').clear_prefix 'SnacksPickerInput'
+
+vim.api.nvim_set_hl(0, "CursorLine", {bg = "#103B3D"})
+vim.api.nvim_set_hl(0, "FloatBorder", {fg = "#268BD2"})
+vim.api.nvim_set_hl(0, "WhichkeyBorder", {link = "FloatBorder"})
+vim.api.nvim_set_hl(0, "SnacksPickerBorder", {link = "FloatBorder"})
