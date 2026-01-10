2e8a1eec7aa47a52bb789889faa1cf7a905bb735
nguyenleson-cydas <nguyenle.son@cydas.com>
2026-01-10 18:57:37 +0900

    wip

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,16 @@
+local hooks = function(ev)
+  local name, kind = ev.data.spec.name, ev.data.kind
+  if name == 'nvim-treesitter' and (kind == 'install' or kind == 'update') then
+    if not ev.data.active then
+      vim.cmd.packadd 'nvim-treesitter'
+    end
+    vim.cmd 'TSUpdate'
+    require('nvim-treesitter').after_update()
+  end
+end
+
+vim.api.nvim_create_autocmd('PackChanged', { callback = hooks })
+
 vim.pack.add {
   {
     src = 'https://github.com/nvim-treesitter/nvim-treesitter.git',
@@ -127,18 +140,3 @@ require('nvim-treesitter.configs').setup {
     },
   },
 }
-
-vim.api.nvim_create_autocmd('PackChanged', {
-  desc = 'Handle nvim-treesitter updates',
-  group = vim.api.nvim_create_augroup('nvim-treesitter-pack-changed-update-handler', { clear = true }),
-  callback = function(event)
-    if event.data.kind == 'update' then
-      local ok = pcall(vim.cmd, 'TSUpdate')
-      if ok then
-        vim.notify('TSUpdate completed successfully!', vim.log.levels.INFO)
-      else
-        vim.notify('TSUpdate command not available yet, skipping', vim.log.levels.WARN)
-      end
-    end
-  end,
-})
